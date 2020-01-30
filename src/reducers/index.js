56278c94f2fe370caae54c090152c9b2bdf3e95c
SerYaroslav <seredyukua@gmail.com>
2020-01-30 22:05:33 +0200

    test

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+  field: {},
+};
+
+const reducer = (state = initialState, action) => {
+
+  switch(action.type){
+    
+
+    default:
+      return state;
+  }
+}
+
+export default reducer;
